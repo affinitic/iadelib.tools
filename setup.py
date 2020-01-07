4a1e7edc87cb23194f72111c4ce7d2866c18da74
Piret Valentin <valentin.piret@affinitic.be>
2020-01-07 11:27:30 +0100

    Back to development: 1.0a2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="iadelib.tools",
-    version="1.0a1",
+    version="1.0a2.dev0",
     description="An add-on for Plone",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
